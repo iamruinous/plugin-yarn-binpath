3436207497811a5569e74fd9464e63b8f35c0916
Jade Meskill <jade.meskill@gmail.com>
2017-07-21 11:43:54 -0700

    fix set, v1.0.1

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
 function init -a path --on-event init_yarn-binpath
-  set -l yarn_bin_path=(yarn global bin)
+  set -l yarn_bin_path (yarn global bin)
   contains -- $yarn_bin_path $PATH
     or set -gx PATH $yarn_bin_path $PATH
 end
