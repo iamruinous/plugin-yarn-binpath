51bb74a7fc509167091b0abeb0faff960dfe477f
Jade Meskill <jade.meskill@gmail.com>
2017-11-02 20:02:27 -0700

    remove init event

--- init.fish
+++ init.fish
@@ -1,5 +1,4 @@
-function init -a path --on-event init_yarn-binpath
-  set -l yarn_bin_path (yarn global bin)
-  contains -- $yarn_bin_path $PATH
-    or set -gx PATH $yarn_bin_path $PATH
-end
+set -l yarn_bin_path (yarn global bin)
+
+contains -- $yarn_bin_path $PATH
+  or set -gx PATH $yarn_bin_path $PATH
